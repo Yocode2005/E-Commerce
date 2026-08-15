a89ff5c3478b8d893df68fc7d56ef9425437ddd3
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-08-15 17:57:54 +0530

    add conditional rendering for signed-in and signed-out states in RootStoreLayout

--- app/store/layout.jsx
+++ app/store/layout.jsx
@@ -1,17 +1,28 @@
 import StoreLayout from "@/components/store/StoreLayout";
+import { Show, SignIn } from "@clerk/nextjs";
 
 export const metadata = {
     title: "GoCart. - Store Dashboard",
     description: "GoCart. - Store Dashboard",
 };
 
-export default function RootAdminLayout({ children }) {
-
+export default function RootStoreLayout({ children }) {
     return (
         <>
-            <StoreLayout>
-                {children}
-            </StoreLayout>
+            <Show when="signed-in">
+                <StoreLayout>
+                    {children}
+                </StoreLayout>
+            </Show>
+
+            <Show when="signed-out">
+                <div className="min-h-screen flex items-center justify-center">
+                    <SignIn
+                        routing="hash"
+                        fallbackRedirectUrl="/store"
+                    />
+                </div>
+            </Show>
         </>
     );
 }
